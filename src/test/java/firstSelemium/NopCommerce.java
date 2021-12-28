04fd3444250e34d3a19a3d8499651e41d1889bb3
waleedafifi90 <waleedafifi.blog@gmail.com>
2021-12-28 13:29:49 +0200

    check the aside, get catalog li by xpath and then click to expand

--- src/test/java/firstSelemium/NopCommerce.java
+++ src/test/java/firstSelemium/NopCommerce.java
@@ -1,7 +1,10 @@
 package firstSelemium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class NopCommerce {
 
@@ -13,6 +16,22 @@ public class NopCommerce {
 		driver.get(url);
 		driver.manage().window().maximize();
 		
+		Assert.assertTrue(driver.getCurrentUrl().contains("login"));
+		
+		WebElement emailField = driver.findElement(By.cssSelector("input[id=\"Email\"]"));
+		WebElement passwordField = driver.findElement(By.cssSelector("input[id=\"Password\"]"));
+		
+		WebElement loginBtn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
+		loginBtn.click();
+		
+		Assert.assertTrue(driver.getCurrentUrl().contains("Admin"));
+		
+		WebElement aside = driver.findElement(By.cssSelector("aside"));
+		
+		Assert.assertTrue(aside.isDisplayed(), "Aside visibility");
+		
+		WebElement catalogLink = aside.findElement(By.xpath("//aside//nav/ul/li/a/*[contains(text(),'Catalog')]/ancestor::a"));
+		catalogLink.click();
 		
 	}
 
